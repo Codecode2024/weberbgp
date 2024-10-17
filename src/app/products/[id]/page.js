8b4738053536034d06abe621725a195f8631400e
Codecode2024 <weberb2024@yahoo.com>
2024-10-18 00:12:33 +0800

    Final_GP_v4

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import styles from "./id.module.css";
 
 const ProductDetail = ({ params }) => {
   const { id } = params;
@@ -21,58 +22,47 @@ const ProductDetail = ({ params }) => {
   if (!product) return <p className="text-center m-5">Loading ...</p>;
 
   return (
-    <Card style={{ border: "none" }}>
-      <div
-        className="container-xxl py-5"
-        style={{
-          background:
-            "linear-gradient(90deg,  rgba(178, 178, 178, 1) 0%, rgba(67, 67, 67, 1) 35%, rgba(7, 7, 7, 1) 100%)",
-        }}
-      >
-        <div className="container">
-          <div className="p-3 pb-md-4 m-auto text-center">
-            <h1
-              className="display-6 fw-normal pt-4"
-              style={{
-                paddingTop: "30px",
-                marginTop: "30px",
-                color: "white",
-              }}
-            >
-              產品詳情
-            </h1>
-            <div className="card mt-5 mb-5 m-auto" style={{ maxWidth: 940 }}>
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <Image
-                    className="img-fluid object-fit-xxl-contain w-100 h-auto rounded"
-                    variant="top"
-                    src={product.image}
-                    width={160}
-                    height={60}
-                    alt={product.name}
-                    priority="{true}"
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body text-start">
-                    <h5 className="card-title text-center py-4">
-                      {product.name}
-                    </h5>
-                    <p className="card-text lh-lg">{product.description}</p>
-                    <p className="card-text">
-                      <strong className="text-danger">
-                        HK${product.price}
-                      </strong>
-                    </p>
+    <section>
+      <Card className={styles.bg}>
+        <div className="container-xxl py-5">
+          <div className="container">
+            <div className="p-3 pb-md-4 m-auto text-center">
+              <h1 className={`display-6 fw-normal pt-4 ${styles.section}`}>
+                產品詳情
+              </h1>
+              <div className="card mt-5 mb-5 m-auto" style={{ maxWidth: 940 }}>
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <Image
+                      className="img-fluid object-fit-xxl-contain w-100 h-auto rounded"
+                      variant="top"
+                      src={product.image}
+                      width={160}
+                      height={60}
+                      alt={product.name}
+                      priority="{true}"
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body text-start">
+                      <h5 className="card-title text-center py-4">
+                        {product.name}
+                      </h5>
+                      <p className="card-text lh-lg">{product.description}</p>
+                      <p className="card-text">
+                        <strong className="text-danger">
+                          HK${product.price}
+                        </strong>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </section>
   );
 };
 
